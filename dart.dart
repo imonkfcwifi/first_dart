dd4154dce24a3e97f6cd59f831848d87f96fe98f
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-07 15:49:14 +0900

    const

--- dart.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  late final String name;
-// do something go to api
-  name = 'imonkfcwifi';
-  print(name);
-
-  // flutter로 data fetching 할때 유용하다
-}
---
+++ main.dart
@@ -0,0 +1,7 @@
+void main() {
+  const max_allowed_price = 120;
+
+  // flutter로 data fetching 할때 유용하다
+}
+// 사용자가 화면에 입력해야 하는 값이라면 final or var
+//  앱스토어에 앱을 올리기 전 (컴파일단계)에 값을 알고있다면 const
