e66d7b29e34c839589b739d3967b332c26cbb884
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-08 11:12:48 +0900

    Collection For

--- main.dart
+++ main.dart
@@ -1,9 +1,13 @@
 void main() {
-  var name = 'imonkfcwifi';
-  var age = 23;
-  var greeting = 'halo my name is $name ! and my age is ${age + 2}';
-
-  print(greeting);
-  // $뒤에 변수 사용
-  // i'm 에 ' 가 들어있기 때문에 문제가 될 수 있으므로 I\'m으로 해줘야 함
+  var oldf = ['hyun', 'roh'];
+  var newf = [
+    'im',
+    'on',
+    'kfc',
+    'wifi',
+    for (var friend in oldf) "❤ $friend",
+  ];
+  print(newf);
+  // collection for은 UI 인터페이스를 만들때 매우 유용하다
 }
+// collection if는 로그인을 했는지 안했는지와 같이 버튼 추가 기능이 있음
