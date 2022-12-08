13a7bf3d0f0f8903c9d428a67bc8b04682a1dd93
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-08 10:54:09 +0900

    collection if

--- main.dart
+++ main.dart
@@ -1,9 +1,14 @@
 void main() {
-  String name = 'imonkfcwifi';
-  bool alive = true;
-  int age = 25;
-  double myage = 25.9;
-  num x = 12;
-  x = 1.1;
-  // X는 double과 int 둘다 가능
+  var giveMeFive = true;
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5,
+  ];
+  // 마지막에 ','를 넣어주면 저절로 여러줄로 포메팅이 된다.
+  print(numbers);
+//  dart 의 list는 collection if와 collection for을 지원한다.
+// collection if는 존재할수도 안할수도 있는 요소를 가지고 만들수있다
 }
