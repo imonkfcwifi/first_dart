6ea98f6c0e226fae952f2f131094bf7c63849748
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-08 11:07:13 +0900

    String Interpolation

--- main.dart
+++ main.dart
@@ -1,14 +1,9 @@
 void main() {
-  var giveMeFive = true;
-  var numbers = [
-    1,
-    2,
-    3,
-    4,
-    if (giveMeFive) 5,
-  ];
-  // 마지막에 ','를 넣어주면 저절로 여러줄로 포메팅이 된다.
-  print(numbers);
-//  dart 의 list는 collection if와 collection for을 지원한다.
-// collection if는 존재할수도 안할수도 있는 요소를 가지고 만들수있다
+  var name = 'imonkfcwifi';
+  var age = 23;
+  var greeting = 'halo my name is $name ! and my age is ${age + 2}';
+
+  print(greeting);
+  // $뒤에 변수 사용
+  // i'm 에 ' 가 들어있기 때문에 문제가 될 수 있으므로 I\'m으로 해줘야 함
 }
