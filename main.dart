9ede3dbed2c4683c5666dced37b27678668f3f24
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-08 15:58:36 +0900

    QQ Operator

--- main.dart
+++ main.dart
@@ -1,11 +1,18 @@
-//
-String sayHello(String name, int age, [String? country = 'cuba']) =>
-    'hello $name youare $age your $country ';
-//  ? = not required
+import 'dart:_js_annotations';
+
+String capiName(String? name) => name?.toUpperCase() ?? 'ANON';
 void main() {
-  var result = sayHello(
-    'imonkfcwifi0',
-    25,
-  );
-  print(result);
+  capiName('imonkfcwifi');
+  capiName(null);
 }
+
+//  left ?? right
+//  left가 null 이면 right을 return
+//  left가 not null 이면 left를 return 한다
+
+void main() {
+  String? name;
+  name ??= 'kfc';
+}
+
+// 만약 name 이 null 이라면 ??=를 써서 다음과 같은 값을 할당해준다
