4520ca4b7754bcf3e8abaa450fe524605dfcab32
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-08 14:42:20 +0900

    Named Parameters

--- main.dart
+++ main.dart
@@ -1,10 +1,12 @@
-String sayHello(String name) => "hello $name nice to meet you";
-// 한줄짜리 코드가 곧바로 return 할경우 fat arrow syntax를 사용한다 아래와 같다
-// String sayHello(String name) {
-//   return "hello $name nice to meet you";
-// }
+String sayHello(
+    {required String name, required int age, required String country}) {
+  return 'halo $name you are $age and you come from $country';
+}
 
-num plus(num a, num b) => a + b;
 void main() {
-  print(sayHello('imonkfcwifi'));
+  print(sayHello(
+    age: 12,
+    country: 'korea',
+    name: 'imonkfcwifi',
+  ));
 }
