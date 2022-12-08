2ab57fa6b7f174eec2b9f1caea37c36a108fc1fe
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-08 19:02:14 +0900

    Constructor

--- main.dart
+++ main.dart
@@ -1,6 +1,8 @@
 class Player {
-  final String name = 'imonkfcwifi';
-  int xp = 1500;
+  final String name;
+  int xp;
+
+  Player(this.name, this.xp);
 
   void sayHello() {
     print("hi my name is $name");
@@ -8,9 +10,13 @@ class Player {
 }
 
 void main() {
-  var player = Player();
-  player.sayHello();
+  var player = Player("imonkfcwifi", 1500);
   print(player.name);
+  player.sayHello();
+  var player2 = Player("roh", 2500);
+  print(player2.name);
+  player2.sayHello();
 }
 
 // class를 생성할때는 타입을 꼭 명시해야 함
+// constructor method의 이름은 class의 이름과 같아야 함
