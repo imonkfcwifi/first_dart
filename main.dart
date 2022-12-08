a4e4fe794a7be5d66c71a025afd9064eecd3215f
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-08 11:21:14 +0900

    Maps

--- main.dart
+++ main.dart
@@ -1,13 +1,24 @@
 void main() {
-  var oldf = ['hyun', 'roh'];
-  var newf = [
-    'im',
-    'on',
-    'kfc',
-    'wifi',
-    for (var friend in oldf) "❤ $friend",
-  ];
-  print(newf);
-  // collection for은 UI 인터페이스를 만들때 매우 유용하다
+  Map<List<int>, bool> player = {
+    [
+      1,
+      2,
+      3,
+      4,
+    ]: true,
+    [
+      4,
+      5,
+      6,
+      7,
+    ]: false,
+    [
+      3,
+      8,
+      7,
+    ]: true,
+    // 명시적으로 특정해줄수 있음
+    // 단순히 var을 써도 상관없음 대신 컴파일러가 key와 value의 자료형을 설정해줌
+    // map도 마찬가지로 method와 property가 있음
+  };
 }
-// collection if는 로그인을 했는지 안했는지와 같이 버튼 추가 기능이 있음
