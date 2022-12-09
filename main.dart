dabc80a8dcfff05d48732f807c3c1d8de1537236
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-09 11:11:57 +0900

    named constructors 2

--- main.dart
+++ main.dart
@@ -1,33 +1,27 @@
 class Player {
   final String name;
-  int xp;
+  int xp, age;
   String team;
-  int age;
 
   Player(
       {required this.name,
       required this.xp,
       required this.team,
       required this.age});
+  // class를 호출할때마다 기본으로 호출되는 기본 constructor
 
-  void sayHello() {
-    print("hi my name is $name");
-  }
+  Player.createBluePlayer({required String name, required int age})
+      : this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
 }
 
+//  콜론(:) 을 넣음으로써 dart에게 객체를 초기화 하겠다 명령
 void main() {
-  var player = Player(
+  var player = Player.createBluePlayer(
     name: "imonnkfcwifi",
-    xp: 1200,
-    team: 'red',
     age: 25,
   );
-  // named parameter 방식 이제 argument들의 위치를 기억하지 않아도 된다
-
-  var player2 = Player("roh", 2500, 'blue', 24);
-
-  // named paramter 방식이 아닐때
+// 각각의 variable(변수)를 클래스에 보내면 기본 constructor에서 class property에 할당함
 }
-
-// class를 생성할때는 타입을 꼭 명시해야 함
-// constructor method의 이름은 class의 이름과 같아야 함 
