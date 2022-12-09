7faeceab66dc5b6bb378ca53dcfc5567fd33ca31
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-09 19:22:48 +0900

    mixins

--- main.dart
+++ main.dart
@@ -1,7 +1,16 @@
+class Strong {
+  final double strengthLevel = 1500.99;
+}
+
+class QuickRunner {
+  void runQuick() {
+    print("run faster run");
+  }
+}
+
 class Human {
   final String name;
   Human({required this.name});
-  // Human 이라는 생성자 함수가 Human에 name 이라는 변수를 넣어준다
   void sayHello() {
     print('hi $name');
   }
@@ -9,30 +18,27 @@ class Human {
 
 enum Team { blue, red }
 
-class Player extends Human {
+class Player extends Human with Strong, QuickRunner {
   final Team team;
   Player({
     required this.team,
-    // Player 이라는 생성자 함수가 Player에 team 이라는 변수를 넣어준다
     required String name,
-    // Player 자체에 있는 team은 그대로 놔두고 name은 super 클래스(부모, extends Human)에 전달해준다.
   }) : super(name: name);
-  // Human(this.name) 으로 못씀 ㅠ named argument를 사용하는 Player의 생성자 함수를 만들어줘야 함
   @override
   void sayHello() {
     super.sayHello();
     print('i play for ${team}');
   }
-  // Human에서 온 sayHello를 우리가 직접만든 method로 대체 한다는 뜻
 }
 
-// super라는 키워드를 통해 확장을 한 부모 클래스(extends Human)와 상호작용
-// 확장(상속)한 부모 클래스의 프로퍼티에 접근하게 해주거나 메소드를 호출할 수 있게 해줌
 void main() {
   var player = Player(
     team: Team.red,
     name: 'Player',
   );
 }
-//  생성자 함수 Player에 있는 name은 Human으로 전달해준다
-//
+
+// Mixin은 생성자가 없는 클래스를 의미한다
+// 클래스에 프로퍼티들을 추가할때 사용한다
+// extends와 차이점은 extend를 하게 되면 확장한 그 클래스는 부모 클래스가 되지만 with는 부모의 인스턴스 관계가 된다. 
+// 단순하게 mixin 내부의 프로퍼티를 갖고 오는 거라고 생각하면 쉽다.
