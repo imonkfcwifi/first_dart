34187fb40a97fb205ce4566071f17b4cfbb9fb14
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-09 15:17:06 +0900

    enum

--- main.dart
+++ main.dart
@@ -1,7 +1,12 @@
+enum Team { red, blue }
+// 오타와 같은 실수를 줄여준다. 텍스트형태 ("")로 쓸 필요도 없음
+// 새로운 타입을 만들어낸다
+
 class Player {
   String name;
   int age;
-  String team;
+  Team team;
+  // 더이상 String team 이 아닌 Team team 이다.
 
   Player({
     required this.name,
@@ -15,10 +20,10 @@ class Player {
 }
 
 void main() {
-  var imonkfcwifi = Player(name: 'roh', age: 1, team: 'e')
+  var imonkfcwifi = Player(name: 'roh', age: 1, team: Team.red)
     ..name = 'new'
     ..age = 2
-    ..team = 'a'
+    ..team = Team.blue
     ..sayHello();
 }
 
