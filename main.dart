96d46ec8b84f1f388d886430268b3b3cad7c908d
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-09 09:53:07 +0900

    named parameter

--- main.dart
+++ main.dart
@@ -1,8 +1,14 @@
 class Player {
   final String name;
   int xp;
+  String team;
+  int age;
 
-  Player(this.name, this.xp);
+  Player(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
 
   void sayHello() {
     print("hi my name is $name");
@@ -10,13 +16,18 @@ class Player {
 }
 
 void main() {
-  var player = Player("imonkfcwifi", 1500);
-  print(player.name);
-  player.sayHello();
-  var player2 = Player("roh", 2500);
-  print(player2.name);
-  player2.sayHello();
+  var player = Player(
+    name: "imonnkfcwifi",
+    xp: 1200,
+    team: 'red',
+    age: 25,
+  );
+  // named parameter 방식 이제 argument들의 위치를 기억하지 않아도 된다
+
+  var player2 = Player("roh", 2500, 'blue', 24);
+
+  // named paramter 방식이 아닐때
 }
 
 // class를 생성할때는 타입을 꼭 명시해야 함
-// constructor method의 이름은 class의 이름과 같아야 함
+// constructor method의 이름은 class의 이름과 같아야 함 
